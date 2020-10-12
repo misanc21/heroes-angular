57d555e541e544d497ef7df0ae99bc6af57c373f
misanc21 <misanc21@gmail.com>
2020-10-12 16:17:27 -0500

    mostrar heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//rutas
+import {app_routing} from './app.routes'
+//servicios
+import {HeroesService} from './services/heroes.service'
+//Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -13,12 +19,16 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HeroesService, Heroe} from '../../services/heroes.service'
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[] = []
 
-  ngOnInit(): void {
+  constructor(private _heroesservice:HeroesService) {
+
+  }
+
+  ngOnInit() {
+    this.heroes = this._heroesservice.getHeroes()
+    console.log(this.heroes)
   }
 
 }
